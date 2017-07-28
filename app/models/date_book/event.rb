3aff7cfcece4d8989ff4f09e238fece040d40aff
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-07-28 11:20:18 -0600

    Devise, CanCanCan, Rolify installed. Initial migrations working.

--- app/models/date_book/event.rb
+++ app/models/date_book/event.rb
@@ -1,5 +1,10 @@
 module DateBook
   class Event < ApplicationRecord
+    # FriendlyId Gem
+    extend FriendlyId
+    friendly_id :name, use: :slugged
+
+    # Schedulable Gem
     acts_as_schedulable :schedule
   end
 end
