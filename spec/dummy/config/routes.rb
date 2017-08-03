794d73c601d6751f6a58020c26ad73a9607f3e7e
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-08-03 08:32:04 -0600

    Cleaned up whitespace

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,21 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
   mount DateBook::Engine => '/date_book', as: 'date_book'
   devise_for :users
   
