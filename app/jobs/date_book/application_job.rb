d6f35125dd1fb7fb6d91e006ec2bd259203528bc
Karen Lundgren <karen.e.lundgren@gmail.com>
2017-08-12 09:28:03 -0600

    Removed unused files

--- app/jobs/date_book/application_job.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module DateBook
-  class ApplicationJob < ActiveJob::Base
-  end
-end
--- app/mailers/date_book/application_mailer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module DateBook
-  # Some documentation goes here
-  class ApplicationMailer < ActionMailer::Base
-    default from: 'from@example.com'
-    layout 'mailer'
-  end
-end
